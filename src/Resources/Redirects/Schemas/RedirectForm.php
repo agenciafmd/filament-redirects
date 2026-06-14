e81a652db35fd5badabcd7077e9b4fec9a84980f
Irineu Junior <irineu@fmd.ag>
2026-06-13 21:57:25 -0300

    refactor(src): organizar schema com Grid e Group

--- src/Resources/Redirects/Schemas/RedirectForm.php
+++ src/Resources/Redirects/Schemas/RedirectForm.php
@@ -9,6 +9,8 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Group;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
@@ -18,45 +20,53 @@ final class RedirectForm
     {
         return $schema
             ->components([
-                Section::make(__('General'))
+                Grid::make(3)
                     ->schema([
-                        Select::make('type')
-                            ->translateLabel()
-                            ->options([
-                                '301' => __('Permanente (301)'),
-                                '302' => __('Temporário (302)'),
-                            ])
-                            ->required()
-                            ->default('301'),
-                        TextEntry::make('placeholder')
-                            ->hiddenLabel(),
-                        TextInput::make('from')
-                            ->translateLabel()
-                            ->prefix(config('app.url'))
-                            ->dehydrateStateUsing(function (?string $state): string {
-                                return '/' . mb_trim($state, '/');
-                            })
-                            ->required(),
-                        TextInput::make('to')
-                            ->translateLabel()
-                            ->url()
-                            ->required(),
-                    ])
-                    ->collapsible()
-                    ->columns()
-                    ->columnSpan(2),
-                Section::make(__('Information'))
-                    ->schema([
-                        Toggle::make('is_active')
-                            ->translateLabel()
-                            ->default(true)
+                        Group::make([
+                            Section::make(__('General'))
+                                ->schema([
+                                    Select::make('type')
+                                        ->translateLabel()
+                                        ->options([
+                                            '301' => __('Permanente (301)'),
+                                            '302' => __('Temporário (302)'),
+                                        ])
+                                        ->required()
+                                        ->default('301'),
+                                    TextEntry::make('placeholder')
+                                        ->hiddenLabel(),
+                                    TextInput::make('from')
+                                        ->translateLabel()
+                                        ->prefix(config('app.url'))
+                                        ->dehydrateStateUsing(function (?string $state): string {
+                                            return '/' . mb_trim($state, '/');
+                                        })
+                                        ->required(),
+                                    TextInput::make('to')
+                                        ->translateLabel()
+                                        ->url()
+                                        ->required(),
+                                ])
+                                ->collapsible()
+                                ->columns()
+                                ->columnSpan(2),
+                        ])
                             ->columnSpan(2),
-                        DateTimeEntry::make('created_at'),
-                        DateTimeEntry::make('updated_at'),
+                        Group::make([
+                            Section::make(__('Information'))
+                                ->schema([
+                                    Toggle::make('is_active')
+                                        ->translateLabel()
+                                        ->default(true)
+                                        ->columnSpan(2),
+                                    DateTimeEntry::make('created_at'),
+                                    DateTimeEntry::make('updated_at'),
+                                ])
+                                ->collapsible()
+                                ->columns(),
+                        ]),
                     ])
-                    ->collapsible()
-                    ->columns(),
-            ])
-            ->columns(3);
+                    ->columnSpanFull(),
+            ]);
     }
 }
